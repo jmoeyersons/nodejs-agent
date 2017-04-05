9832ef6d581a5a5e066f2fca4cb34f19fc9f04b6
Jerico Moeyersons <jerico.moeyersons@ugent.be>
2017-04-05 10:14:12 +0200

    Fixed issue with ip_addr variable

--- userdata_workers.sh
+++ userdata_workers.sh
@@ -8,5 +8,5 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs 
 sudo npm install
 curl -L -s http://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- \
-        -i faafo -r worker -e 'http://$ip_addr' -m 'amqp://faafo:guest@$ip_addr:5672/'
+        -i faafo -r worker -e "http://$ip_addr" -m "amqp://faafo:guest@$ip_addr:5672/"
 sudo npm start 
