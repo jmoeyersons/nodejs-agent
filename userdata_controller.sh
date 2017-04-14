826fdc1f511027144f28ded85edeead80f309288
Jerico Moeyersons <jerico.moeyersons@ugent.be>
2017-04-14 15:15:40 +0200

    Removed the node-js agent

--- userdata_controller.sh
+++ userdata_controller.sh
@@ -3,13 +3,13 @@
 cd ~
 git clone https://github.com/moeyerke/nodejs-agent.git
 cd nodejs-agent
-sudo apt install npm -y
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs 
-sudo npm install
+#sudo apt install npm -y
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get install -y nodejs 
+#sudo npm install
 curl -L -s http://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- -i messaging -i faafo -r api
 sudo su
 rabbitmqctl add_user faafo guest
 rabbitmqctl set_user_tags faafo administrator
 rabbitmqctl set_permissions -p / faafo ".*" ".*" ".*"
-npm start 
+#npm start 
--- userdata_workers.sh
+++ userdata_workers.sh
@@ -13,14 +13,14 @@ echo "Ip-adress is $ip_addr"
 cd ~
 git clone https://github.com/moeyerke/nodejs-agent.git
 cd nodejs-agent
-sudo apt install npm -y
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs 
-sudo npm install
+#sudo apt install npm -y
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get install -y nodejs 
+#sudo npm install
 echo "Going to sleep for 2min so the controller is ready..."
 sleep 2m
 curl -L -s http://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- \
         -i faafo -r worker -e "http://$ip_addr" -m "amqp://faafo:guest@$ip_addr:5672/"
 sleep 10s
 supervisorctl restart faafo_worker
-sudo npm start 
+#sudo npm start 
